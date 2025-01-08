f6e50b2bcde67d34c7fa60d878da62c6a8024b70
Cheng Hao <2000016838@stu.pku.edu.cn>
2025-01-09 01:43:31 +0800

    Update scraping3_answer_meta_by_questionID.py

--- codes/scraping3_answer_meta_by_questionID.py
+++ codes/scraping3_answer_meta_by_questionID.py
@@ -1,8 +1,10 @@
 # Get answer posts and answer post meta-data by question ID
 
-# Input: A list of question ID
-# Output: A list of answer posts and answer post meta-data by question ID: Answer post text, answer date, upvote count, comment count, answer ID, author name, author gender, author follower count, author bio, author username
 # Last Run: 2024/11/16 12:55
+# About this script:
+# **Input**: A list of question ID
+# **Output**: A list of answer posts and answer post meta-data by question ID: Answer post text, answer date, upvote count, comment count, answer ID, author name, author gender, author follower count, author bio, author username
+# Last Run: 2025/1/9 1:38
 
 import re
 import os
@@ -16,62 +18,65 @@ def get_q_list(filename):
     df = pd.read_csv(filename, encoding="utf-8")
     df = df[df["answerCount"] > 5]  # 默认爬取回答数大于5的问题
     df = df[df["created_date"] >= "2024-10-15"]  # 可选要更新的问题的时间范围
-    q_list = df.values.tolist()
-    print(f"共有 {len(q_list)} 个回答数大于5且不重复的问题")
+    
+    questions_dict = dict(zip(df["q_id"].tolist(), df["q_content"].tolist()))
+    
+    print(f"共有 {len(questions_dict)} 个回答数大于5且不重复的问题")
 
-    return q_list[::-1]  # 从后往前爬
+    return dict(reversed(questions_dict.items()))   # 从后往前爬
 
-def parse_data(html, q_id):
-
-    json_data = json.loads(html)["data"]
-    next_url = json.loads(html)["paging"]["next"]
-    is_end = json.loads(html)["paging"]["is_end"]
+def parse_data(url, q_id):
+    text = get_url_text(url)
+    
+    try:
+        json_data = json.loads(text)["data"]
+        next_url = json.loads(text)["paging"]["next"]
+        is_end = json.loads(text)["paging"]["is_end"]
+    except Exception as e:
+        print(f"Error: {url}")
+        print(e)
 
     one_q_all_answer = []
 
-    try:
-        for item in json_data:
-            one_answer_list = []
+    for item in json_data:
+        one_answer_list = []
 
-            question_id = q_id  # Question id
-            answer_content = item["target"]["content"]
-            answer_content = re.sub("<[^<]+?>", "", answer_content)
-            answer_date = datetime.fromtimestamp(item["target"]["created_time"]).strftime(
-                "%Y-%m-%d"
-            )  # Answer date
-            answer_upvote = item["target"]["voteup_count"]  # upvote count
-            answer_comment = item["target"]["comment_count"]  # comment count
-            answer_id = item["target"]["id"]  # answer ID
-            author_name = item["target"]["author"]["name"]  # author name
-            author_gender = item["target"]["author"][
-                "gender"
-            ]  # author gender, 1 male 2 female
-            author_url_token = item["target"]["author"]["url_token"]  # author ID
-            author_follower_count = item["target"]["author"][
-                "follower_count"
-            ]  # author follower count
-            author_headline = item["target"]["author"]["headline"]  # author bio
+        question_id = q_id  # Question id
+        answer_content = item["target"]["content"]
+        answer_content = re.sub("<[^<]+?>", "", answer_content)
+        answer_date = datetime.fromtimestamp(item["target"]["created_time"]).strftime(
+            "%Y-%m-%d"
+        )  # Answer date
+        answer_upvote = item["target"]["voteup_count"]  # upvote count
+        answer_comment = item["target"]["comment_count"]  # comment count
+        answer_id = item["target"]["id"]  # answer ID
+        author_name = item["target"]["author"]["name"]  # author name
+        author_gender = item["target"]["author"][
+            "gender"
+        ]  # author gender, 1 male 2 female
+        author_url_token = item["target"]["author"]["url_token"]  # author ID
+        author_follower_count = item["target"]["author"][
+            "follower_count"
+        ]  # author follower count
+        author_headline = item["target"]["author"]["headline"]  # author bio
 
-            one_answer_list = [
-                question_id,
-                answer_content,
-                answer_date,
-                answer_upvote,
-                answer_comment,
-                answer_id,
-                author_name,
-                author_gender,
-                author_url_token,
-                author_follower_count,
-                author_headline,
-            ]
-            one_q_all_answer.append(one_answer_list)
+        one_answer_list = [
+            question_id,
+            answer_content,
+            answer_date,
+            answer_upvote,
+            answer_comment,
+            answer_id,
+            author_name,
+            author_gender,
+            author_url_token,
+            author_follower_count,
+            author_headline,
+        ]
+        one_q_all_answer.append(one_answer_list)
 
-        return one_q_all_answer, next_url, is_end
-
-    except Exception as e:
-        print(one_q_all_answer)
-        print(e)
+    return one_q_all_answer, next_url, is_end
+    
 
 def save_data(answer_info, q_id):
 
@@ -102,16 +107,17 @@ def save_data(answer_info, q_id):
 
 if __name__ == "__main__":
     # TODO: 指定问题列表
-    q_list = get_q_list("data/question_meta_info.csv")
+    questions_dict = get_q_list("data/question_meta_info.csv")
+    q_id_list = list(questions_dict.keys())
     # 也可手动输入问题 ID 以获取回答数据
-    # q_list = ["291278869", "291278870"]
+    # q_list = ["24324127", "24399025"]
 
-    # 爬一段时间会触发知乎的验证码机制导致HTTPError报错，需要手动重新设置开始位置
+    # 爬一段时间会触发京东的验证码机制导致HTTPError报错，需要手动重新设置开始位置
     begin_index = 0  # 将发生报错的问题序号更新到这里即可
-    for i, item in enumerate(q_list[begin_index:]):  
-        q_id = item[0]
+    for i, q_id in enumerate(q_id_list[begin_index:]):
+        q_content = questions_dict.get(q_id, "None")
 
-        print(f"\nquestion {i+begin_index} {item[1]} Begin, qid: {q_id}")
+        print(f"\nquestion {i+begin_index} {q_content} Begin, qid: {q_id}")
 
         url = f"https://www.zhihu.com/api/v4/questions/{str(q_id)}/feeds?include=content%2Cauthor.follower_count"
 
@@ -126,8 +132,7 @@ if __name__ == "__main__":
 
             try:
                 # 已有数据的旧问题尝试按时间排序，节省时间
-                text = get_url_text(url + "&order=updated")
-                data, url, is_end = parse_data(text, q_id)
+                data, url, is_end = parse_data(url + "&order=updated", q_id)
                 url = url + "&order=updated"
                 get_data_by_time = True
             except:
@@ -136,13 +141,12 @@ if __name__ == "__main__":
 
         # 对于回答数很多的问题，报错时可在此处添加中途url，方便断点续爬
         # url = "" # 放入报错前最后输出的url
-        #TODO
+        # TODO
 
         page = 0
         is_end = False
         while not is_end:
-            text = get_url_text(url)
-            data, url, is_end = parse_data(text, q_id)
+            data, url, is_end = parse_data(url, q_id)
 
             save_data(data, q_id)
 
@@ -161,6 +165,6 @@ if __name__ == "__main__":
                 except:
                     pass
 
-        print(f"\nquestion {i+begin_index} {item[1]} Finish")
+        print(f"\nquestion {i+begin_index} {q_content} Finish")
 
     print("Finish!!")
